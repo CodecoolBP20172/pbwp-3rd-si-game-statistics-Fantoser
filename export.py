a69ee220364d8ec615c644a983bc3b5334e88b3f
Kurucz Péter <k.fantoser@gmail.com>
2017-07-04 22:41:10 +0200

    Done

--- export.py
+++ export.py
@@ -1,2 +1,25 @@
-
 # Export functions
+from reports import *
+
+
+def export_list(function, filename):
+    exportlist = function
+    for i in exportlist:
+        filename.write(i + ", ")
+    filename.write("\n")
+
+
+def export(filename="gamestat_report.txt"):
+    filename = open("gamestat_report.txt", "w")
+
+    filename.write(str(count_games("game_stat.txt")) + "\n")
+    filename.write(str(decide("game_stat.txt", 2011)) + "\n") 
+    filename.write(str(get_latest("game_stat.txt")) + "\n") 
+    filename.write(str(count_by_genre("game_stat.txt", "First-person shooter")) + "\n") 
+    filename.write(str(get_line_number_by_title("game_stat.txt", "Half-Life")) + "\n")
+    export_list(sort_abc("game_stat.txt"), filename)
+    export_list(get_genres("game_stat.txt"), filename)
+    filename.write(str(when_was_top_sold_fps("game_stat.txt")) + "\n")     
+    filename.close()
+
+export()
--- reports.py
+++ reports.py
@@ -1,2 +1,79 @@
 
 # Report functions
+# readcsv = csv.reader(csvfile, delimiter=' ')
+
+
+def import_file(filename):
+    datas = []
+    thefile = open(filename)
+    for line in thefile:
+        datas.append(line.split("\t"))
+    return datas
+
+
+def count_games(filename):
+    datas = import_file(filename)
+    return len(datas)
+
+
+def decide(filename, year):
+    datas = import_file(filename)
+    for game in datas:
+        if int(game[2]) == year:
+            return True
+    return False
+
+
+def get_latest(filename):
+    year = 0
+    gamename = ""
+    datas = import_file(filename)
+    for game in datas:
+        if int(game[2]) > year:
+            year = int(game[2])
+            gamename = game[0]
+    return gamename
+
+
+def count_by_genre(filename, genre):
+    genrecounter = 0
+    datas = import_file(filename)
+    for game in datas:
+        if game[3] == genre:
+            genrecounter += 1
+    return genrecounter
+
+
+def get_line_number_by_title(filename, title):
+    linecounter = 0
+    datas = import_file(filename)
+    for game in datas:
+        linecounter += 1
+        if game[0] == title:
+            return linecounter
+
+
+def sort_abc(filename):
+    datas = import_file(filename)
+    titles = []
+    for game in datas:
+        titles.append(game[0])
+    return sorted(titles)
+
+def get_genres(filename):
+    datas = import_file(filename)
+    genres = []
+    for game in datas:
+        genres.append(game[3])
+    return sorted(set(genres),key = str.lower)
+
+def when_was_top_sold_fps(filename):
+    datas = import_file(filename)
+    soldcounter = 0
+    release_year= 0
+    for game in datas:
+        if game[3] == "First-person shooter":
+            if float(game[1]) > soldcounter:
+                soldcounter = float(game[1])
+                release_year = game[2]
+    return int(release_year)
